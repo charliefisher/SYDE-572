21a6b4d630e9bed64631c23b9cb62449535597ae
Charlie Fisher <charliefisher@users.noreply.github.com>
2023-03-29 19:48:44 -0400

    Implement parzen estimation; seems correct but still needs to be verified

--- Lab 2/model_estimation.py
+++ Lab 2/model_estimation.py
@@ -40,22 +40,46 @@ class EstimationMethod(Enum):
 
 """Main code for part 1 of the lab"""
 def estimate_model_1d(method: EstimationMethod, data: np.ndarray, true_dist: Distribution) -> None:
+    # parametric and nonparamtric estimations are dispatched to different methods 
+    # since plotting is different for each estimation method
+    if method == EstimationMethod.PARZEN:
+        _non_parametric_estimate_1d(method, data, true_dist)
+    else:
+        _parametric_estimate_1d(method, data, true_dist)
+
+"""Parametric estimate for part 1 of the lab"""
+def _parametric_estimate_1d(method: EstimationMethod, data: np.ndarray, true_dist: Distribution) -> None:
     ax, x_range = _plot_true_distribution(data, true_dist)
 
     if method == EstimationMethod.GAUSSIAN:
         mean, std = np.mean(data), np.std(data)
-        _plot_guassian(ax, x_range, mean, std, label='Estimated Distribution', color='tab:orange')
+        _plot_guassian(ax, x_range, mean, std, 'Estimated Distribution', 'tab:orange')
     elif method == EstimationMethod.EXPONENTIAL:
         lambda_ = 1 / np.mean(data)
-        _plot_exponential(ax, x_range, lambda_, label='Estimated Distribution', color='tab:orange')
+        _plot_exponential(ax, x_range, lambda_, 'Estimated Distribution', 'tab:orange')
     elif method == EstimationMethod.UNIFORM:
         a, b = np.min(data), np.max(data)
-        _plot_uniform(ax, x_range, a, b, label='Estimated Distribution', color='tab:orange')
-    
+        _plot_uniform(ax, x_range, a, b, 'Estimated Distribution', 'tab:orange')
+    else:
+        raise ValueError("Unexpected value for estimation method")
+
     ax.legend()
     _save_figure('part1/{est}_estimate_for_{true}.png'.format(
         est=method.name.lower(), true=true_dist.type.name.lower()
     ))
+
+"""Non-parametric estimate for part 1 of the lab"""
+def _non_parametric_estimate_1d(method: EstimationMethod, data: np.ndarray, true_dist: Distribution) -> None:
+    if method == EstimationMethod.PARZEN:
+        for guassian_window_std in [0.1, 0.4]:
+            ax, x_range = _plot_true_distribution(data, true_dist)
+            _plot_parzen(ax, x_range, data, guassian_window_std, 'Estimated Distribution', 'tab:orange')
+            ax.legend()
+            _save_figure('part1/{est}_{std}_estimate_for_{true}.png'.format(
+                est=method.name.lower(), std=str(guassian_window_std), true=true_dist.type.name.lower()
+            ))
+    else:
+        raise ValueError("Unexpected value for estimation method")
 
 
 """Main code for part 2 of the lab"""
@@ -77,16 +101,17 @@ def _plot_true_distribution(
         true_dist: Distribution) -> tuple[matplotlib.axes.Axes, tuple[float, float]]:
 
     # compute histogram bins
-    counts, bins = np.histogram(data)
+    counts, bins = np.histogram(data, bins=40)
     # plot histogram with normal distribution overlaid on seperate y axis
     _, ax = plt.subplots()
     ax.hist(bins[:-1], bins, weights=counts, fc=[*matplotlib.colors.to_rgb('b'), 0.4])
-    ax.set_xlabel('Measured Distance (cm)')
     ax.set_ylabel('Occurence')
     ax.set_ylim(bottom=0)
 
     # return seperate y axis to plot probability distribution on
     ax2 = ax.twinx()
+    ax2.set_ylabel('Probability Density')
+    ax2.set_ylim(bottom=0)
     x_range = (bins[0], bins[-1])
 
     # plot true distribution
@@ -111,8 +136,6 @@ def _plot_guassian(
     dist_y = scipy.stats.norm(loc=mean, scale=std).pdf(dist_x)
             
     ax.plot(dist_x, dist_y, color, label=label)
-    ax.set_ylabel('Probability Density')
-    ax.set_ylim(bottom=0)
 
 """Plots Exponential PDF"""
 def _plot_exponential(
@@ -124,8 +147,6 @@ def _plot_exponential(
     dist_y = scipy.stats.expon(scale=1/lambda_).pdf(dist_x)
             
     ax.plot(dist_x, dist_y, color, label=label)
-    ax.set_ylabel('Probability Density')
-    ax.set_ylim(bottom=0)
 
 
 """Plots Uniform PDF"""
@@ -147,5 +168,20 @@ def _plot_uniform(
     dist_y = scipy.stats.uniform(loc=a, scale=b-a).pdf(dist_x)
             
     ax.plot(dist_x, dist_y, color, label=label)
-    ax.set_ylabel('Probability Density')
-    ax.set_ylim(bottom=0)
+
+"""Plots Parzen Estimation"""
+def _plot_parzen(
+        ax: matplotlib.axes.Axes, x_range: tuple[float, float],
+        data: np.ndarray, guassian_window_std: float,
+        label: Optional[str] = None, color: Optional[str] = 'g') -> None:
+
+    N = len(data.T)  # NOTE: data is a 1xN matrix
+    h = guassian_window_std
+
+    dist_x = np.linspace(*x_range)
+    dist_x_rep = np.repeat(dist_x.reshape((len(dist_x), 1)), N, axis=1)
+    data_rep = np.repeat(data, len(dist_x), axis=0)
+
+    dist_y = 1 / N * np.sum(1 / h * scipy.stats.norm().pdf((dist_x_rep - data_rep) / h), axis=1)
+            
+    ax.plot(dist_x, dist_y, color, label=label)
